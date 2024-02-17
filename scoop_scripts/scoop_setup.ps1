56c1fa2fc08f06c54c291c9927ec203b5cad939b
kuru-emre <kuru.emre@hotmail.com>
2024-02-17 17:13:31 -0400

    Updated
    Signed-off-by: kuru-emre <kuru.emre@hotmail.com>

--- scoop_scripts/scoop_setup.ps1
+++ scoop_scripts/scoop_setup.ps1
@@ -4,7 +4,14 @@ if (-not (Test-Path $env:USERPROFILE\scoop)) {
 }
 
 # Read the list of apps from the text file
+$bucketsToInstall = Get-Content "buckets.txt"
 $appsToInstall = Get-Content "apps.txt"
+
+
+# Install buckets
+foreach ($bucket in $bucketsToInstall) {
+    scoop bucket add $bucket
+}
 
 # Install each app
 foreach ($app in $appsToInstall) {
